53d29bab07fdb83ce6ae61854a67f29b56fd41aa
Anmol Ratan Tiwari <anmolratantiwari@coer.ac.in>
2025-03-03 13:03:00 +0530

    rest api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,56 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const app = express();
+const User = require("./models/user");
+app.use(express.json());
+app.delete("/feed", async (req, res) => {
+  const userDetail = req.body.email;
+  try{
+  const  userData = await User.deleteOne({email : userDetail});
+  if(userData.length === 0) {
+    res.status(400).send("Email Not Found");
+  }
+    res.send(userData);
+}
+catch(err){
+  res.status(400).send("Email Not Found");
+}
+});
+app.post("/signup", async (req, res) => {
+ // console.log(req.body);
+  const user = new User(req.body);
+    
+    console.log("Attempting to save user:", user);
+    try {
+    const savedUser = await user.save();
+    console.log("User saved successfully:", savedUser);
+    res.send({ message: "Data Entered Successfully", user: savedUser });
+    }
+    catch (err) {
+      console.error("Error saving user:", err);
+      res.status(500).send("Error saving user");
+    }
+});
+app.get("/feed", async (req, res) => {
+  const userDetail = req.body.email;
+  try{
+  const  userData = await User.find( {});
+  if(userData.length === 0) {
+    res.status(400).send("Email Not Found");
+  }
+    res.send(userData);
+}
+  catch(err){
+    res.status(400).send("Email Not Found");
+  }
+});
+
+
+
+
+
+
+
 connectDB().then(()=>{
   console.log("Connection Successful");
   app.listen(8888, () => {
@@ -9,23 +60,4 @@ connectDB().then(()=>{
   console.log("Connection Failed");
 });
 
-const app = express();
-app.use("/home",(req,res)=>{
-  res.send("welcome to the homepage");
-});
 
-app.get("/user",(req,res,next)=>{
-  //This is a route handler
-// res.send("Sending from respond");
-  res.send("This is it");
-  });
-
-app.use("/login",(req,res)=>{
-    res.send("Enter your username and password");
-  });
-  app.use("/signup",(req,res)=>{
-    res.send("Create your account");
-  });
-  app.use("/feed",(req,res)=>{
-    res.send("Swipe Left and Right");
-  });
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 connectDB=async() => {
     await mongoose.connect(
-    "mongodb+srv://artiwari10:Dfxecmg73MXIfvSj@nodejslearning.njduk.mongodb.net/?retryWrites=true&w=majority&appName=NodejsLearning/CampusDevMatch");
-};
+"mongodb+srv://artiwari10:Dfxecmg73MXIfvSj@nodejslearning.njduk.mongodb.net/?retryWrites=true&w=majority&appName=NodejsLearning"
+)};
 
 module.exports = connectDB;
